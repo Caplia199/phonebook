2671e46f47599c2b78603867d259f7edbe01c571
Artem Rodionov <a.rodionov@pulkovo-service.ru>
2023-08-27 13:59:49 +0300

    First steps with socet 2

--- src/server/soket/socket.js
+++ src/server/soket/socket.js
@@ -32,5 +32,32 @@
 // module.exports = { setupSocket };
 
 
+// Вот так правильнее
 
+// const WebSocket = require('ws');
+// const number = require('../controllers/Controllers');
+
+// function initializeWebSocket(server) {
+//   const wss = new WebSocket.Server({ server });
+
+//   wss.on('connection', (ws) => {
+//     console.log('WebSocket connected');
+
+//     ws.on('message', async (message) => {
+//       try {
+//         const data = JSON.parse(message);
+//         if (data.type === 'newNumber') {
+//           const result = await number.postNumber(data.data);
+//           console.log('Received newNumber:', data.data);
+
+//           ws.send(JSON.stringify({ type: 'numberSaved', data: 'Number saved successfully' }));
+//         }
+//       } catch (error) {
+//         console.error('Error processing client message:', error);
+//       }
+//     });
+//   });
+// }
+
+// module.exports = initializeWebSocket;
 
